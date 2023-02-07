01fc21a0dde225255f6b82f998d4e463435c0d11
svyat174 <110224503+svyat174@users.noreply.github.com>
2023-02-07 22:13:11 +0300

    create button component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
-import { Htag } from "@/components";
+import { Button, Htag } from "@/components";
 import React from "react";
 
 export default function Home(): JSX.Element {
   return (
-    <div>
+    <>
       <Htag tag="h3">Volosova</Htag>
-    </div>
+      <Button appearance="primary">button</Button>
+      <Button appearance="ghost">button</Button>
+    </>
   );
 }
