65a53343b02b10e2f5558e70ee004a7cc6e050c6
svyat174 <110224503+svyat174@users.noreply.github.com>
2023-02-07 19:10:33 +0300

    add tags and start creating button

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,20 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import '../styles/globals.css'
+import Head from "next/head";
+import React from "react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+export default function Home({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <>
+      <Head>
+        <title>Volosova - обучение, студия наращивания волос</title>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet"/>
+      </Head>
+      <Component { ...pageProps } />
+    </>
+  )
 }
