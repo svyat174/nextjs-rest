d67bf8c21e4b812ac898e7851fd4610ea4628ce7
svyat174 <110224503+svyat174@users.noreply.github.com>
2023-02-08 22:11:58 +0300

    create footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,12 +1,17 @@
 import styles from './Footer.module.css'
 import cn from "classnames"
 import { FooterProps } from "./Footer.props"
+import { format } from 'date-fns'
 
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
   return (
-    <div {...props}>
-      Footer
-    </div>
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div>
+        Hair Club © 2022 - {format(new Date(), 'yyyy')} Все права защищены
+      </div>
+      <a href="#" target='_blank'>User rules</a>
+      <a href="#" target='_blank'>Confidencial politics</a>
+    </footer>
   )
 }
