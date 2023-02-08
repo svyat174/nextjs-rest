ca5a099a0bbb1c732f7796a371c6bc733ef52df1
svyat174 <110224503+svyat174@users.noreply.github.com>
2023-02-08 15:29:57 +0300

    add wp config, button, paragraph and tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,19 @@
-import { Button, Htag } from "@/components";
-import React from "react";
+import React from 'react';
+import { Button, Htag, P, Tag } from '../components';
 
 export default function Home(): JSX.Element {
-  return (
-    <>
-      <Htag tag="h3">Volosova</Htag>
-      <Button appearance="primary">button</Button>
-      <Button appearance="ghost">button</Button>
-    </>
-  );
+	return (
+		<>
+			<Htag tag='h1'>Текст</Htag>
+			<Button appearance='primary' arrow='right'>Кнопка</Button>
+			<Button appearance='ghost' arrow='down'>Кнопка</Button>
+      <P size='l'>Big</P>
+      <P>Medium</P>
+      <P size='s'>Small</P>
+      <Tag size='s'>Ghost</Tag>
+      <Tag size='m' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Green</Tag>
+      <Tag color='primary'>Primary</Tag>
+		</>
+	);
 }
