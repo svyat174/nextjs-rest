2693c522524a1a8a2dfab5a513d9330e0d221ce1
svyat174 <110224503+svyat174@users.noreply.github.com>
2023-02-08 18:04:25 +0300

    add Rating Component

---
+++ components/Rating/Rating.tsx
@@ -0,0 +1,64 @@
+import { RatingProps } from "./Rating.props"
+import styles from './Rating.module.css'
+import { useEffect, useState, KeyboardEvent } from "react"
+import StarIcon from './star.svg'
+import cn from 'classnames'
+
+export const Rating = ({ isEditable=false, rating, setRating, className, ...props }: RatingProps): JSX.Element => {
+  const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>))
+  
+  useEffect(() => {
+    conctructRating(rating)
+  }, [rating])
+
+  const conctructRating = (currentRating: number) => {
+    const updatedArray = ratingArray.map((r: JSX.Element, i: number) => {
+      return (
+        <span
+        className={
+          cn(styles.fill, {
+            [styles.filled]: i < currentRating,
+            [styles.editable]: isEditable,
+          })
+        }
+        onMouseEnter={() => changeDisplay(i + 1)}
+        onMouseLeave={() => changeDisplay(rating)}
+        onClick={() => onClick(i + 1)}
+        >
+          <StarIcon
+          tabIndex={isEditable ? 0 : -1}
+          onKeyDown={(e: KeyboardEvent<SVGElement>) => isEditable && handleSpace(i + 1, e)}
+        />
+        </span>
+      )
+    })
+    setRatingArray(updatedArray)
+  }
+
+  const changeDisplay = (i: number) => {
+    if (!isEditable) {
+      return;
+    }
+    conctructRating(i)
+  }
+
+  const onClick = (i: number) => {
+    if (!isEditable || !setRating) {
+      return;
+    }
+    setRating(i)
+  }
+
+  const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
+    if (e.code != 'Space' || !setRating) {
+      return;
+    }
+    setRating(i)
+  }
+
+  return (
+    <div {...props}>
+      {ratingArray.map((r, i) => (<span key={i}>{r}</span>))}
+    </div>
+  )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import { Button, Htag, P, Tag } from '../components';
+import React, { useEffect, useState } from 'react';
+import { Button, Htag, P, Rating, Tag } from '../components';
 
 export default function Home(): JSX.Element {
+  const [rating, setRating] = useState<number>(1)
 	return (
 		<>
-			<Htag tag='h1'>Текст</Htag>
+			<Htag tag='h1'>Volosova</Htag>
 			<Button appearance='primary' arrow='right'>Кнопка</Button>
 			<Button appearance='ghost' arrow='down'>Кнопка</Button>
       <P size='l'>Big</P>
@@ -14,6 +15,7 @@ export default function Home(): JSX.Element {
       <Tag size='m' color='red'>Red</Tag>
       <Tag size='s' color='green'>Green</Tag>
       <Tag color='primary'>Primary</Tag>
+      <Rating rating={rating} isEditable setRating={setRating}/>
 		</>
 	);
 }
